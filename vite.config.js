f4467b2893548353aff98a9886e79a076ceb078e
Kevinhan1 <kevinss.xs@gmail.com>
2025-07-07 15:31:21 +0700

    perbaika vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import path from 'path'
+// import path from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'  // Ganti require dengan import
+import laravel from 'laravel-vite-plugin'
+// import autoprefixer from 'autoprefixer'  // Ganti require dengan import
 
 export default defineConfig({
   plugins: [
+    laravel({
+      input: ['/resources/css/app.css', '/resources/js/app.js'],
+      refresh: true,
+    }),
     tailwindcss(),  // Pastikan Tailwind CSS ada di plugin
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './resources'),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(), 
-        autoprefixer(),  // Gunakan autoprefixer dengan import
-      ],
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './resources'),
+  //   },
+  // },
+  // css: {
+  //   postcss: {
+  //     plugins: [
+  //       tailwindcss(), 
+  //       // autoprefixer(),  // Gunakan autoprefixer dengan import
+  //     ],
+  //   },
+  // },
 })
-node -v
+// node -v
